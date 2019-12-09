3b6dcd5dd3267433e949fc56ec8d081c98c7c053
sukhwanipawan <58626094+sukhwanipawan@users.noreply.github.com>
2019-12-09 22:23:46 +0530

    Update Wiki_Headings.py

--- Wiki_Headings.py
+++ Wiki_Headings.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup                                   #Here we are importing package called 'bs4'
+from bs4 import BeautifulSoup                                   #Here we are importing package called 'Beautiful Soup'
 import requests                                                 #Here we are importing package called 'requests' which will be used for requesting webpage
 reg1 = requests.get('https://en.wikipedia.org/wiki/Python')     #Requesting a webpage on wikipedia
 reg2 = BeautifulSoup(reg1.text, 'lxml')                         #This BeautifulSoup will take two parameters, first is object reg1.text and second is structure we want
