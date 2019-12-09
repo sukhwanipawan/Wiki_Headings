1d5339205df8a3f413a51bb8d9d852bd27d5dbfe
sukhwanipawan <58626094+sukhwanipawan@users.noreply.github.com>
2019-12-09 22:36:52 +0530

    Update Step_Title.py

--- Step_Title.py
+++ Step_Title.py
@@ -1,6 +1,6 @@
-import requests                               
-import bs4
-var = requests.get('https://stepapp.ai/')
-cont1 = bs4.BeautifulSoup(var.text, 'lxml')
-count2 = cont1.select('title')
+import requests                               #Here we are importing package called 'requests' which will be used for requesting webpage
+import bs4                                    #Here we are importing package called 'Beautiful Soup'
+var = requests.get('https://stepapp.ai/')     #Requesting a webpage on wikipedia
+cont1 = bs4.BeautifulSoup(var.text, 'lxml')   #This BeautifulSoup will take two parameters, first is object var.text and second is structure we want
+count2 = cont1.select('title')                
 print(count2)
